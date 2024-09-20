795b581445cf3084cff0bcce597cd5a10d786825
johnpaulmacanas <202380256@psu.palawan.edu.ph>
2024-09-21 01:51:35 +0800

    Initial commit with Heron's Formula and Leap Year Checkerconsole apps

---
+++ Practice01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the Legths of the Triangle");
+            Console.Write("Enter the 1st side:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 2nd side:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the 3rd side:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalcuArea(a, b, c);
+
+            Console.WriteLine($"The area of the triangle is {area}");
+        }
+
+        static double CalcuArea(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+}
---
+++ Practice2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            bool isLeapYear = LeapYear(year);
+            if (isLeapYear)
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+
+        static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
